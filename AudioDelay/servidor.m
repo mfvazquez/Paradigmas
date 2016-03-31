7fa32df757e958c6bec16b99539ac7f1e6dcc85e
mfvazquez <mfvazquez@fi.uba.ar>
2016-03-31 16:17:41 -0300

    hice unos arreglos a AudioDelay\servidor.m

--- AudioDelay/servidor.m
+++ AudioDelay/servidor.m
@@ -1,7 +1,8 @@
  global parallel_port;
       
-pportaddr = 'C020';
-% pportaddr = '378';
+
+ pportaddr = '378';
+% pportaddr = '0378';
 
 if exist('pportaddr','var') && ~isempty(pportaddr)
 
@@ -17,47 +18,43 @@ if exist('pportaddr','var') && ~isempty(pportaddr)
 
 end
 
-
-input_data=io32(pportobj,pportaddr);
-bit1_old = bitand(input, 1);
-bit2_old = bitand(input, 2);
+while true
+    input_data=io32(pportobj,pportaddr)
+end
+ 
+% input_data=io32(pportobj,pportaddr);
+% bit1_old = bitand(input_data, 1);
+% bit2_old = bitand(input_data, 2);
+% bit1_change = 0;
+% bit2_change = 0;
+% input_data_old=input_data
 % while true
-%     input_data=io32(pportobj, pportaddr);
-%     bit1 = bitand(input, 1);
-%     bit2 = bitand(input, 2);
-%     if (bit1_old ~= bit1)
+% 
+%     input_data=io32(pportobj, pportaddr)
+%     if (input_data ~= input_data_old)
+%         input_data
+%         input_data_old = input_data;
+%     end
+%     bit1 = bitand(input_data, 1);
+%     bit2 = bitand(input_data, 2);
+%     if (bit1 == 0)
+%         bit1_old = 0;
+%     end
+%     if (bit2 == 0)
+%         bit2_old = 0;
+%     end
+%     
+%     if (bit1 == 1 && bit1_old == 0)
 %         bit1_change = GetSecs
 %         bit1_old = bit1;
 %     end
-%     if (bit2_old ~= bit2)
+%     if (bit2 == 1 && bit2_old == 0)
 %         bit2_change = GetSecs
 %         bit2_old = bit2;
 %     end
+%     if (bit1_change ~= 0 && bit2_change ~= 0)
+%         delay = bit1_change - bit2_change
+%         bit1_change = 0;
+%         bit2_change = 0;
+%     end
 % end
-bit1_change = 0;
-bit2_change = 0;
-while true
-    input_data=io32(pportobj, pportaddr);
-    bit1 = bitand(input, 1);
-    bit2 = bitand(input, 2);
-    if (bit1 == 0)
-        bit1_old = 0;
-    end
-    if (bit2 == 0)
-        bit2_old = 0;
-    end
-    
-    if (bit1 == 1 && bit1_old == 0)
-        bit1_change = GetSecs
-        bit1_old = bit1;
-    end
-    if (bit2 == 1 && bit2_old == 0)
-        bit2_change = GetSecs
-        bit2_old = bit2;
-    end
-    if (bit1_change ~= 0 && bit2_change ~= 0)
-        delay = bit1_change - bit2_change
-        bit1_change = 0;
-        bit2_change = 0;
-    end
-end
