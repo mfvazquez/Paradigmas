a0c1c94daec7f977747a506097e37450e628cec1
mfvazquez <mfvazquezfiuba@gmail.com>
2016-05-17 10:37:25 -0300

    agregadas las historias del paradigma moral

---
+++ Moral/prueba historias/bin/textoCentradoBoton.m
@@ -0,0 +1,14 @@
+function textoCentradoBoton(texto)
+
+    global hd;
+    
+    [~, screenYpixels] = Screen('WindowSize', hd.window);
+    
+    textSize = round(screenYpixels*0.035);
+
+    Screen('TextSize', hd.window, textSize);
+    DrawFormattedText(hd.window, texto, 'center','center', hd.white, [],[],[], 2,[],[]);
+    Screen('Flip', hd.window);
+    
+
+end
