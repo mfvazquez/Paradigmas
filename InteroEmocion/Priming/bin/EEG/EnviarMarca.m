0f3c881c6568bd2277f7bea143b48a757f22a0ed
mfvazquez <mfvazquezfiuba@gmail.com>
2016-11-16 08:05:37 -0300

    asd

--- InteroEmocion/Priming/bin/EEG/EnviarMarca.m
+++
@@ -1,9 +0,0 @@
-function EnviarMarca(marca)
-
-    global pportobj pportaddr MARCA_DURACION
-
-    io32(pportobj,pportaddr, marca);
-    WaitSecs(MARCAS.DURACION);
-    io32(pportobj,pportaddr,0);
-
-end
--- InteroEmocion/Priming/bin/conductual/lib/CargarTextosDeCarpeta.m
+++
@@ -1,9 +0,0 @@
-function textos = CargarTextosDeCarpeta(carpeta)
-   
-    archivos_de_carpeta = ArchivosDeCarpeta(carpeta);
-    textos = cell(1, length(archivos_de_carpeta));
-    for i = 1:length(textos)
-        textos{i} = fileread(fullfile(carpeta, archivos_de_carpeta{i}));
-    end
-
-end
